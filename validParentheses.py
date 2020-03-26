76ee3dceb675255586b4abafde50d12eac5cd194
ting huang <329295821@qq.com>
2020-03-26 16:50:35 +0800

    first commit

---
+++ validParentheses.py
@@ -0,0 +1,27 @@
+
+class Solution:
+    def isValid(self, s):
+        stack = []
+        map = {
+            "{" :"}",
+            "[" :"]",
+            "(" :")"
+        }
+        for x in s:
+            if x in map:
+                stack.append(map[x])
+            else:
+                if len(stack )!= 0:
+                    top_element = stack.pop()
+                    if x != top_element:
+                        return False
+                    else:
+                        continue
+                else:
+                    return False
+        return len(stack) == 0
+
+
+if __name__ == "__main__":
+    test = Solution()
+    print(test.isValid("{}]"))
